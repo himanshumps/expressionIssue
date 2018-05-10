c377ef22e96a5e896737c086cfab00733e50dfa5
Himanshu Gupta <Himanshu.Gupta14@aexp.com>
2018-05-10 10:29:54 +0530

    first commit

---
+++ src/main/java/com/test/swagger/expressionIssue/controller/HomepageController.java
@@ -0,0 +1,16 @@
+package com.test.swagger.expressionIssue.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+public class HomepageController {
+	@ApiOperation(value="The home page. Split character is ${splitChar}", 
+			notes="Notes - The home page. Split character is ${splitChar}")
+	@GetMapping(value={"/"})
+	public String homepage() {
+		return "<html><body>Home page .... <br/>Swagger URL: <a href='/swagger-ui.html'>Click here</a></body</html>";
+	}
+}
